dc37e85373b1ad78c09775f2f2e3b967de15df9c
DAVID-PC\David <oprutadavid@gmail.com>
2018-12-09 18:13:52 +0200

    Lab 5

---
+++ Lab5/Lab5/State.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Client
+{
+    public class State
+    {
+        public const int BUFFER_SIZE = 256;
+
+        // Client socket.  
+        public Socket WorkSocket { get; set; }
+        // Size of receive buffer.  
+
+        // Receive buffer.  
+        public byte[] Buffer { get; set; }
+        // Received data string.  
+        public StringBuilder sBuilder { get; set; }
+
+        public int Index { get; set; }
+        public String Host { get; set; } 
+
+        public State()
+        {
+            sBuilder = new StringBuilder();
+            Buffer = new byte[BUFFER_SIZE];
+        }
+
+        public State(String host, int index)
+        {
+            Host = host;
+            Index = index;
+            sBuilder = new StringBuilder();
+            Buffer = new byte[BUFFER_SIZE];
+        }
+    }
+}
+
